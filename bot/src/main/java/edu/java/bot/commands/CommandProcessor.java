c8813e69bc003bfa916231226f5d8ed3efbb394f
Zer-01 <oko48ru@gmail.com>
2024-02-18 22:16:38 +0300

    rework command search

--- bot/src/main/java/edu/java/bot/commands/CommandProcessor.java
+++ bot/src/main/java/edu/java/bot/commands/CommandProcessor.java
@@ -3,32 +3,43 @@ package edu.java.bot.commands;
 import com.pengrad.telegrambot.model.Update;
 import edu.java.bot.botUtils.SendMessageRequest;
 import edu.java.bot.db.Database;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CommandProcessor {
-    List<Command> commands;
+    Map<String, Command> commands;
 
     public CommandProcessor(Database database) {
-        commands = List.of(
-            new HelpCommand(),
-            new ListCommand(database),
-            new StartCommand(database),
-            new TrackCommand(database),
-            new UntrackCommand(database)
-        );
+        commands = new HashMap<>();
+
+        Command tmp = new HelpCommand();
+        commands.put(tmp.command(), tmp);
+
+        tmp = new ListCommand(database);
+        commands.put(tmp.command(), tmp);
+
+        tmp = new StartCommand(database);
+        commands.put(tmp.command(), tmp);
+
+        tmp = new TrackCommand(database);
+        commands.put(tmp.command(), tmp);
+
+        tmp = new UntrackCommand(database);
+        commands.put(tmp.command(), tmp);
     }
 
     public SendMessageRequest process(Update update) {
         String firstWord = update.message().text().split(" ")[0];
-        for (Command command : commands) {
-            if (command.command().equals(firstWord)) {
-                return command.handle(update);
-            }
+        Command command = commands.get(firstWord);
+        if (command != null) {
+            return command.handle(update);
+        } else {
+            return null;
         }
-        return null;
     }
 
     public List<Command> commands() {
-        return commands;
+        return commands.values().stream().toList();
     }
 }
