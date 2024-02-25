905a294119d555d76dbe9f9df9b2363e43cb994d
Zer-01 <oko48ru@gmail.com>
2024-02-25 17:16:18 +0300

    add task 3

---
+++ scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
@@ -0,0 +1,14 @@
+package edu.java;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LinkUpdaterScheduler {
+    @Scheduled(fixedDelayString = "${app.scheduler.interval}")
+    void update() {
+        log.info("Updated");
+    }
+}
--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -6,8 +6,10 @@ import edu.java.configuration.ClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties({ApplicationConfig.class, ClientConfiguration.class, BaseUrlsConfig.class})
 public class ScrapperApplication {
     public static void main(String[] args) {
