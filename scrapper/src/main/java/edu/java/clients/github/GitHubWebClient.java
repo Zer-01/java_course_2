b9c858ad7f07ab0f3897bb40bdaac7f855a04849
Zer-01 <oko48ru@gmail.com>
2024-02-27 14:34:05 +0300

    add retry and timeout to WebClients

--- scrapper/src/main/java/edu/java/clients/github/GitHubWebClient.java
+++ scrapper/src/main/java/edu/java/clients/github/GitHubWebClient.java
@@ -1,6 +1,7 @@
 package edu.java.clients.github;
 
 import edu.java.dto.RepositoryResponse;
+import java.time.Duration;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,6 +11,8 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 public class GitHubWebClient implements GitHubClient {
     private final static String DEFAULT_BASE_URL = "https://api.github.com";
     private final WebClient webClient;
+    private final static long NUMBER_OF_ATTEMPTS = 5;
+    private final static long TIMEOUT_SECONDS = 5;
 
     public GitHubWebClient(String baseUrl) {
         if (baseUrl == null) {
@@ -31,6 +34,8 @@ public class GitHubWebClient implements GitHubClient {
                 .uri("/repos/{owner}/{repo}", owner, repoName)
                 .retrieve()
                 .bodyToMono(RepositoryResponse.class)
+                .timeout(Duration.ofSeconds(TIMEOUT_SECONDS))
+                .retry(NUMBER_OF_ATTEMPTS)
                 .blockOptional();
         } catch (WebClientResponseException e) {
             log.error("GitHUb client error: " + e.getMessage());
--- scrapper/src/main/java/edu/java/clients/stackoverflow/StackOverflowWebClient.java
+++ scrapper/src/main/java/edu/java/clients/stackoverflow/StackOverflowWebClient.java
@@ -1,6 +1,7 @@
 package edu.java.clients.stackoverflow;
 
 import edu.java.dto.QuestionResponse;
+import java.time.Duration;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,6 +11,8 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 public class StackOverflowWebClient implements StackoverflowClient {
     private final static String DEFAULT_BASE_URL = "https://api.stackexchange.com";
     private final WebClient webClient;
+    private final static long NUMBER_OF_ATTEMPTS = 5;
+    private final static long TIMEOUT_SECONDS = 5;
 
     public StackOverflowWebClient(String baseUrl) {
         webClient = WebClient.builder()
@@ -28,6 +31,8 @@ public class StackOverflowWebClient implements StackoverflowClient {
                 .uri("/questions/{id}?site=stackoverflow", questionId)
                 .retrieve()
                 .bodyToMono(QuestionResponse.class)
+                .timeout(Duration.ofSeconds(TIMEOUT_SECONDS))
+                .retry(NUMBER_OF_ATTEMPTS)
                 .blockOptional();
         } catch (WebClientResponseException e) {
             log.error("StackOverflow client error: " + e.getMessage());
