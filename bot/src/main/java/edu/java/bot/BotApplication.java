34adb5085266e1d65b558a1cdafd4c90407e6031
Zer-01 <oko48ru@gmail.com>
2024-03-31 17:41:23 +0300

    add rate limiting for bot

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -6,8 +6,10 @@ import edu.java.bot.configuration.WebClientsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 @EnableConfigurationProperties({ApplicationConfig.class, WebClientsConfig.class, ClientConfiguration.class})
 public class BotApplication {
     public static void main(String[] args) {
